86cbb36058351f71c00e8ed66cb5cd2fd4668234
wys <wuyushan1989@163.com>
2018-06-21 10:42:15 +0800

    Create IdentityUser class
    Create User class and database context

--- IdentityTest/Models/IdentityModel.cs
+++ IdentityTest/Models/IdentityModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,19 @@ namespace IdentityTest.Models
 {
     public class AppUser : IdentityUser
     {
+        public AppUser(string userName) :base(userName)
+        {
+
+        }
+        public DateTime DateOfBirth { get; set; }
+    }
+
+    public class AppIdentityDbContext:IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext() : base("IdentityTestDb") { }
+        public static AppIdentityDbContext Create()
+        {
+            return new AppIdentityDbContext();
+        }
     }
 }
