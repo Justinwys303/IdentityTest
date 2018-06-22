25d237583495ea589432b6dde748886de7181f73
wys <wuyushan1989@163.com>
2018-06-22 15:55:43 +0800

    Add Login
    Add Login

--- IdentityTest/Controllers/AccountController.cs
+++ IdentityTest/Controllers/AccountController.cs
@@ -55,12 +55,40 @@ namespace IdentityTest.Controllers
             return View(model);
         }
 
-        //[AllowAnonymous]
-        //public ActionResult Login(string returnUrl)
-        //{
-        //    ViewBag.ReturnUrl = returnUrl;
-        //    return View();
-        //}
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                return View("Error", new string[] { "您已经登录！" });
+            }
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await UserManager.FindAsync(model.Email, model.Password);
+                if(user == null)
+                {
+                    ModelState.AddModelError("", "无效的用户名和密码");
+                }
+                else
+                {
+                    var claimsIdentity = await UserManager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie);
+                    //AuthManager.SignOut();
+                    //AuthManager.SignIn(new AuthenticationProperties { IsPersistent = false }, claimsIdentity);
+                    return Redirect(returnUrl);
+                }
+            }
+            ViewBag.returnUrl = returnUrl;
+            return View(model);
+        }
 
         private void AddErrors(IdentityResult result)
         {
